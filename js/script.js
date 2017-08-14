f018c2131f615352e349b2fc81e1e108f042d661
J.R. Ruggiero <fleury14@comcast.net>
2017-08-14 12:02:23 -0700

    Almost finished first puzzle, just need to finish cpu actions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+var wordList = ['rhythmic', 'photon', 'adverb', 'infinite', 'discourage'];
+// word list for all puzzles
+
+// Global var init: buzzer status, timer status, puzzle answer
+var buzzer = false;
+var buzzerInstance = 0;
+var stopTimeFlag = false;
+var currentWord = '';
+var boardControl = 0;
+
+// declare puzzle box ID
+var puzzleBox = document.getElementById('puzzle-box');
+
+
 let today = new Date(); // Get todays date and loale
 let locale = "en-us";
 
@@ -73,4 +87,173 @@ function firstPuzzleStart() {
   gameConsole.innerHTML = "";
 
   gameConsole.innerHTML = 'First puzzle commencing...';
-}
+
+  var randNum = Math.floor(Math.random()*(wordList.length - 1));
+  currentWord = wordList[randNum]; // pull an random word for the list
+
+
+// Output pizzle answer for debug
+  // var wordOutput = document.createElement('p');
+  // wordOutput.innerHTML = currentWord;
+  // gameConsole.appendChild(wordOutput);
+
+  for (i = 0; i < currentWord.length; i++) {
+
+    // Create the tiles for the puzzle. Give each box the class for the box styles and an ID with a corresponding number (letterbox*). Finally, output the boxes
+    var boxIdOutput = 'letter-box' + i.toString();
+    var boxOutput = document.createElement('div');
+    boxOutput.setAttribute('class', 'puzzle-letter-box');
+    boxOutput.setAttribute('id', boxIdOutput);
+    puzzleBox.appendChild(boxOutput);
+  }
+
+  // Variable prepartion: reset timer flags and buzzer, string checker and get the length of the current answer
+  // Also, prepare buzzer instance so we know what to do when the buzzer is activate (0 = 1st puzzle, 1 = bonus round etc)
+
+  var cpuPercent = 0;
+  i = 0;
+  var j = currentWord.length;
+  var letterCheckString = '';
+  var randCPUCheck = 0;
+
+// Create buzzer in console FOR NOW
+
+  insertedElement.setAttribute('class', 'continue-button');
+  insertedElement.setAttribute('onclick', 'playerOneBuzzer()');
+  insertedElement.innerHTML = "BUZZER";
+  gameConsole.appendChild(insertedElement);
+
+
+// Begin timer: start filling in tiles at a rate of 1 per second
+  var fillInTimer = setInterval(fillInTiles, 1000);
+
+// Loop until either a buzzer is triggered or all but one of the tiles are filled
+  function fillInTiles() {
+    while (stopTimeFlag==false) {
+
+// Pick a random letter from the current answer
+      var letterTarget = Math.floor(Math.random() * j);
+
+// Make sure that if this is the second to last letter that this will be the final loop
+      if (i == j-2) {
+        clearInterval(fillInTimer);
+        console.log('stop time flag triggered');
+        stopTimeFlag = true;
+      }
+
+// If the current index of the array has been used already, change the random number. Note that because the check consists of appending numbers to an array, any word with more than 10 letters will not work becoase '10' would be appended
+// FUTURE:: Convert the resulting checkstring to hex to allow 16 letter words
+      while(letterCheckString.indexOf(letterTarget) >= 0)
+      {
+         letterTarget = Math.floor(Math.random() * j);
+      }
+
+// Target the box of the letter that will be filled in
+      var elementTarget = 'letter-box' + letterTarget.toString();
+
+// Get the actual letter to be used (Weve been using array numbers to avoid confusion if the same letter is used twice)
+      var letterToBeFilled = currentWord.charAt(letterTarget);
+
+  //     console.log ('j', j, 'lettertarget', letterTarget, 'letterToBeFilled', letterToBeFilled);
+
+// A secondary conditional (probably redundant, remove later) to make sure the letter in question hasn't been used
+// Passing that: Display the letter, increment the loop counter, add the array label to the string checkString, and console log it for debug
+      if (letterCheckString.indexOf(letterTarget) < 0) {
+        document.getElementById(elementTarget).innerHTML = letterToBeFilled;
+        i++;
+        letterCheckString = letterCheckString + letterTarget.toString();
+        console.log('checkstring' + letterCheckString);
+      }
+
+  cpuPercent += 0.1;
+  console.log(cpuPercent * 100 + '%');
+
+  randCPUCheck = Math.random();
+
+  if (randCPUCheck < cpuPercent) {
+    buzzerActivation();
+  }
+
+
+//  Avoid an endless loop
+  break;
+    }
+
+
+  } //End fillInTiles
+
+
+
+
+  // var consanantList = 'bcdfghjklmnpqrstvwxyz';
+  // var vowelList = 'aeiou';
+  //
+  // while (true) {
+  //   randVowel = vowelList.charAt(Math.floor(Math.random()*5));
+  //
+  //   if (currentWord.indexOf(randVowel) < 0) {
+  //     var tileList = currentWord.concat(randVowel);
+  //     console.log(tileList);
+  //     break;
+  //   }
+  //
+  // }
+
+} // End firstPuzzleStart()
+
+// Function if buzzer is clicked
+  function playerOneBuzzer() {
+
+// Make sure a timer is still going, and if so, stop it and change the buzzer flag so we know what caused it
+    if(buzzer==false) {
+      console.log ('time stopped due to buzzer');
+      buzzer=true;
+      stopTimeFlag=true;
+
+      buzzerActivation(); // Call activation function for insctructions
+    }
+  } // End playerOneBuzzer()
+
+function buzzerActivation() {
+
+// Impotant note: the only way this function is activated with the buzzer flag being false should be for CPU activation
+  if (buzzer == false) {
+
+    console.log('CPU Buzzer activation');
+
+    switch(buzzerInstance) {
+
+        case 0:
+          alert('CPU INTERCEPT!');
+          break;
+
+        default:
+          break;
+
+    } // End Buzzer instance switch
+
+  } // End CPU buzzer if
+
+  switch(buzzerInstance) {
+
+    case 0:
+      var playerGuess = prompt('Buzzer Activated! Please input your guess. Remember, spelling counts');
+      var playerResult = playerGuess.toLowerCase();
+
+      console.log('guess:' + playerResult + ' answer:' + currentWord);
+
+      if (playerResult == currentWord) {
+        alert('YOU GOT IT!!!!!!11ONE');
+      }
+
+      else {
+        alert('WRONG-O! Control lost.');
+      }
+
+      break;
+
+    default:
+        break;
+  } // End player buzzer insance switch
+
+} // End buzzerActivation()
